0ba375ab67cce2d05e3917a2026e5a2eed0a1410
Luis Eduardo Alcantara Olvera <geekshinobi@gmail.com>
2020-02-26 22:02:40 -0600

    Cambioamos algunos componentes que estaban con class a funcional components

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,31 +2,24 @@ import React from 'react'
 import circlesImg from './../images/circles.png';
 import './../styles/Card.css';
 
-class Card extends React.Component {
-
-    render(){
-        const {title, description, img, leftColor, rightColor} = this.props
-        return (
-            <div className="card mx-auto Fitness-Card" 
-            style={{
-                backgroundImage: `url(${circlesImg}), linear-gradient(to right, ${leftColor}, ${rightColor})`
-            }}
-            >
-                <div className="card-body">
-                    <div className="row center">
-                        <div className="col-6">
-                            {/* Usamos el state para colocar la imagen */}
-                            <img src={img} alt="imagen" className="float-right"/>
-                        </div>
-                        <div className="col-6 Fitness-Card-Info">
-                            <h1>{title}</h1>
-                            <p>{description}</p>
-                        </div>
+const Card = ({title, description, img, leftColor, rightColor}) => (
+    <div className="card mx-auto Fitness-Card" 
+        style={{
+            backgroundImage: `url(${circlesImg}), linear-gradient(to right, ${leftColor}, ${rightColor})`
+        }}
+        >
+            <div className="card-body">
+                <div className="row center">
+                    <div className="col-6">
+                        {/* Usamos el state para colocar la imagen */}
+                        <img src={img} alt="imagen" className="float-right"/>
+                    </div>
+                    <div className="col-6 Fitness-Card-Info">
+                        <h1>{title}</h1>
+                        <p>{description}</p>
                     </div>
                 </div>
             </div>
-        )
-    }
-}
-
+        </div>
+)
 export default Card;
